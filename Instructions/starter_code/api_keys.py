43aad3645c0c94b380faf1aefe63c82385c179d9
Brian Young <Brian.Young@sacw.com>
2020-10-22 22:20:11 -0500

    gmaps and data loaded into vacation

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "acf83678340ce5afec12d701bcdfc3c3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBnxBhLONnORL5jVjDkryq0_RPA72kghKY"
